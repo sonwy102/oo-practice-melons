0e9d6511f7c948b707967c11ff2a70ed98deb362
Wooyang Son <sonwy102@gmail.com>
2020-10-08 19:54:22 +0000

    Added harvest.py part1

--- harvest.py
+++ harvest.py
@@ -11,38 +11,68 @@ class MelonType(object):
         """Initialize a melon."""
 
         self.pairings = []
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
 
-        # Fill in the rest
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
-
-        # Fill in the rest
+        
+        self.pairings.extend(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
-        # Fill in the rest
+        self.code = new_code
 
 
 def make_melon_types():
     """Returns a list of current melon types."""
 
-    all_melon_types = []
+    muskmelon = MelonType('musk', 1998, 'green', True, True, 'Muskmelon')
+    muskmelon.add_pairing(['mint'])
 
-    # Fill in the rest
+    casaba = MelonType('cas', 2003, 'orange', False, False, 'Casaba')
+    casaba.add_pairing(['strawberries','mint'])
 
+    crenshaw = MelonType('cren', 1996, 'green', False, False, 'Crenshaw')
+    crenshaw.add_pairing(['proscuitto'])
+
+    yellow_watermelon = MelonType('yw', 2013, 'yellow', False, True, 'Yellow Watermelon')
+    yellow_watermelon.add_pairing(['ice cream'])
+
+    all_melon_types = [muskmelon, casaba, crenshaw, yellow_watermelon]
+    
     return all_melon_types
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
-    # Fill in the rest
+    for melon in melon_types:
+        print(f"{melon.name} pairs with")
+        for pairing in melon.pairings:
+            print(f'- {pairing}')
+        
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
-    # Fill in the rest
+    melon_dict = {}
+    for melon in melon_types:
+        
+        # If melon.code doesn't exist as a key in melon_dict
+        if melon.code not in melon_dict:
+            # Initialize key and add <melon object> as an element of the value (list)
+            melon_dict[melon.code] = [melon]
+        else: 
+            # add <melon object> into melon_dict[key]'s value (list)
+            melon_dict[melon.code].append(melon)
+           
+    return melon_dict
 
 ############
 # Part 2   #
