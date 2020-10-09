72f7162fd29860f22c71feec2721a7d9dce2576d
Wooyang Son <sonwy102@gmail.com>
2020-10-09 00:45:29 +0000

    Added harvest.py part2 and further study

--- harvest.py
+++ harvest.py
@@ -8,7 +8,7 @@ class MelonType(object):
 
     def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
                  name):
-        """Initialize a melon."""
+        """Initialize a melon type."""
 
         self.pairings = []
         self.code = code
@@ -63,14 +63,16 @@ def make_melon_type_lookup(melon_types):
 
     melon_dict = {}
     for melon in melon_types:
+
+        melon_dict[melon.code] = melon
         
-        # If melon.code doesn't exist as a key in melon_dict
-        if melon.code not in melon_dict:
-            # Initialize key and add <melon object> as an element of the value (list)
-            melon_dict[melon.code] = [melon]
-        else: 
-            # add <melon object> into melon_dict[key]'s value (list)
-            melon_dict[melon.code].append(melon)
+        # # If melon.code doesn't exist as a key in melon_dict
+        # if melon.code not in melon_dict:
+        #     # Initialize key and add <melon object> as an element of the value (list)
+        #     melon_dict[melon.code] = [melon]
+        # else: 
+        #     # add <melon object> into melon_dict[key]'s value (list)
+        #     melon_dict[melon.code].append(melon)
            
     return melon_dict
 
@@ -81,18 +83,81 @@ def make_melon_type_lookup(melon_types):
 class Melon(object):
     """A melon in a melon harvest."""
 
-    # Fill in the rest
-    # Needs __init__ and is_sellable methods
+    def __init__(self, melon_type, shape_rating, color_rating, field, harvester):
+        """Initialize a melon"""
+
+        self.melon_type = melon_type
+        self.shape_rating = shape_rating
+        self.color_rating = color_rating
+        self.field = field
+        self.harvester = harvester
+
+    def is_sellable(self):
+        """Returns true if melon is sellable, false otherwise"""
+
+        if self.shape_rating > 5 and self.color_rating > 5 and self.field != 3:
+            return True 
+        return False
+
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
 
-    # Fill in the rest
+    # Create dictionary of {reporting code(str): MelonType (obj)}
+    melons_by_id = make_melon_type_lookup(melon_types)
+
+    # Instantiate melons 1 - 9
+    melon1 = Melon(melons_by_id['yw'], 8, 7, 2, 'Sheila')
+    melon2 = Melon(melons_by_id['yw'], 3, 4, 2, 'Sheila')
+    melon3 = Melon(melons_by_id['yw'], 9, 8, 3, 'Sheila')
+    melon4 = Melon(melons_by_id['cas'], 10, 6, 35, 'Sheila')
+    melon5 = Melon(melons_by_id['cren'], 8, 9, 35, 'Michael')
+    melon6 = Melon(melons_by_id['cren'], 8, 2, 35, 'Michael')
+    melon7 = Melon(melons_by_id['cren'], 2, 3, 4, 'Michael')
+    melon8 = Melon(melons_by_id['musk'], 6, 7, 4, 'Michael')
+    melon9 = Melon(melons_by_id['yw'], 7, 10, 3, 'Sheila')
+
+    # Make a list of Melon objects
+    melon_harvest = [melon1, melon2, melon3, melon4, melon5, melon6, melon7, melon8, melon9]
+
+    return melon_harvest
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
-    # Fill in the rest 
+    for melon in melons:
 
+        # Set default state to NOT SELLABLE
+        state = 'NOT SELLABLE'
+
+        # if is_sellable returns True, change state to CAN BE SOLD
+        if melon.is_sellable():
+            state = 'CAN BE SOLD'
+
+        print(f"Harvested by {melon.harvester} from Field {melon.field} ({state})")
+
+def make_melon_obj(filename, melon_types):
+    """Given a txtfile of melons harvested, return a dictionary of each Melon object"""
+
+    # Open txtfile of melons and initialize variables
+    harvest_data = open(filename)
+    index = 1
+    melon_obj_dict = {}
+
+    # Create dictionary of {reporting code(str): MelonType (obj)}
+    melons_by_id = make_melon_type_lookup(melon_types)
+
+    # Iterate over each line of melon_file
+    for line in harvest_data:
+
+        # Process each line of data and add to melon_obj_dict
+        line = line.split(' ')
+        melon_obj_dict["melon" + str(index)] = Melon(melons_by_id[line[5]], line[1], line[3], line[11], line[8])
+        index += 1
+    
+    # Close file
+    harvest_data.close()
+
+    return melon_obj_dict
 
 
